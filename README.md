0c67ab9012198d00cf8753f10ea4bad75166b7ba
Willin Wang <zhilin.wang@wuliangroup.com>
2015-03-25 10:25:18 +0800

    coverall test

---
+++ README.md
@@ -0,0 +1,3 @@
+# WNO
+
+## Installation
---
+++ test/index.js
@@ -0,0 +1,8 @@
+var wno = require('../index');
+
+describe('WNO', function () {
+	'use strict';
+	it('Hello World!', function () {
+		console.log(wno);
+	});
+});
